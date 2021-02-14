48119a3c4429f9aec2a1b41906ba62cfdb430398
r-aravind <aravind.ar786@gmail.com>
2021-02-14 17:51:02 +0530

    update: added missing documentation to index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,8 +1,14 @@
+// Libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// App Component
 import App from './App';
+
+// Stylesheet
 import './index.css';
 
+// Render
 ReactDOM.render(
   <React.StrictMode>
     <App />
